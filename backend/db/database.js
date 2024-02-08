ce65b5eedd52ca1fc6105366cf1aaec5de8849de
sulogna2001 <roysulogna2001@gmail.com>
2024-02-08 18:35:45 +0530

    new env

--- backend/db/database.js
+++ backend/db/database.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "test",
+    password: process.env.DATABASE_KEY,
     database: "todo_db"
 });
 
